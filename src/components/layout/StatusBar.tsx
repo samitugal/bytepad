b45eb3f2f8935930c25d1b2cc9f685769d38f97c
samitugal <tugalsami@gmail.com>
2026-01-17 15:25:44 +0300

    v0.22.2: Add Linux build to release workflow

--- src/components/layout/StatusBar.tsx
+++ src/components/layout/StatusBar.tsx
@@ -58,7 +58,7 @@ export function StatusBar() {
             <span>⚡</span> {language === 'tr' ? 'Çevrimdışı' : 'Offline'}
           </span>
         )}
-        <span>bytepad v0.22.1</span>
+        <span>bytepad v0.22.2</span>
       </div>
     </div>
   )
